cdb1653dd010df7feffaef3a0bf64640d9eec192
pedrofariad1 <pedrofariad1@gmail.com>
2026-05-21 20:25:03 -0300

    Aula 21/5

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,30 +1,50 @@
-import { usuario } from "./bd";
-
+import { usuario } from "./bd.js";
 
 const email = usuario.email;
 const senha = usuario.senha;
 
-
-
 function verificarCredenciais() {
-    const emailInformado = document.getElementById("email") .value;
-    const senhaInformado = document.getElementById("senha") .value;
+
+    const emailInformado =
+        document.getElementById("email").value;
+
+    const senhaInformado =
+        document.getElementById("senha").value;
+
+    if (emailInformado === email) {
+
+        alert("E-mail correto");
+
+        if (senhaInformado === senha) {
+
+            alert("Senha correta");
+
+            window.location.href = "home.html";
+
+        } else {
+
+            alert("Senha errada");
+
+        }
+
+    } else {
+
+        alert("E-mail errado");
+
+    }
+
 }
 
-if (emailInformado === email) {
-    alert("E-mail correto");
-    if (senhaInformado === senha) {
-        alert("Senha correta")
-        window.location = "home.html";
-    } else alert ("Senha errada");
-} else alert ("E-mail errado");
-
 document.addEventListener("DOMContentLoaded", () => {
-    const botao =document.querySelector("button");
+
+    const botao = document.querySelector("button");
+
     botao.addEventListener("click", (e) => {
+
         e.preventDefault();
+
         verificarCredenciais();
+
     });
+
 });
-
-
